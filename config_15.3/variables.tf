190052be5432f62fe0ca2279419205ca8f66543f
s-bessonniy <164614779+s-bessonniy@users.noreply.github.com>
2025-05-24 12:05:47 +0300

    Create variables.tf

---
+++ config_15.3/variables.tf
@@ -0,0 +1,33 @@
+###cloud vars
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+  #default = ""
+  #sensitive = true
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  #default = ""
+  #sensitive = true
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  #default = ""
+  #sensitive = true
+}
+
+variable student_name {
+  description = "Name Of Student"
+  type        = string
+  default     = "S.Yaremko"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
